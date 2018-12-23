bb0d97be5766bfd27b509a6f822a8fef01bc3cd4
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2018-12-23 21:49:55 +0545

    binary search tree efficieant

--- binary_tree/lca2.py
+++ binary_tree/lca2.py
@@ -20,13 +20,16 @@ class BinarySearchTree:
             current = self.root
 
             while True:
+                print(val, current.info)
                 if val < current.info:
+                    print('in left')
                     if current.left:
                         current = current.left
                     else:
                         current.left = Node(val)
                         break
                 elif val > current.info:
+                    print('in right')
                     if current.right:
                         current = current.right
                     else:
@@ -36,6 +39,7 @@ class BinarySearchTree:
                     break
 
 
+# Enter your code here. Read input from STDIN. Print output to STDOUT
 '''
 class Node:
       def __init__(self,info): 
@@ -46,46 +50,24 @@ class Node:
 
        // this is a node of the tree , which contains info as data, left , right
 '''
-hist_a = []
-hist_b = []
-flag = False
 
 
-def lca(root, v1, v2):
-    global hist_a
-    global hist_b
-    global flag
-    hist_a = parents(root, v1, hist_a)
-    flag = False
-    hist_b = parents(root, v1, hist_b)
-    for hist in hist_a:
-        if hist in hist_b:
-            ancestor = hist
-        else:
-            return ancestor
-    return hist_a[len(hist_a) - 1]
+def lca(root, n1, n2):
+    # Base Case
+    if root is None:
+        return None
 
+    # If both n1 and n2 are smaller than root, then LCA
+    # lies in left
+    if (root.info > n1 and root.info > n2):
+        return lca(root.left, n1, n2)
 
-def parents(root, v, histry):
-    global flag
-    if root.left and not flag:
-        histry.append(root)
-        if root.left.info == v:
-            flag = True
-            return histry
-        histry = parents(root.left, v, histry)
-    if root.right and not flag:
-        histry.append(root)
-        if root.right.info == v:
-            flag = True
-            return histry
-        histry = parents(root.right, v, histry)
-    try:
-        histry.pop()
-    except:
-        histry.append(root)
-        return histry
-    return histry
+        # If both n1 and n2 are greater than root, then LCA
+    # lies in right
+    if (root.info < n1 and root.info < n2):
+        return lca(root.right, n1, n2)
+
+    return root
 
 
 if __name__ == '__main__':
