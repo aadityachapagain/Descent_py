9a9972fa3f57d864334f8ca62ede72e2ab31bae7
aaditya <aaditya@fusemachines.com>
2019-07-24 19:34:00 +0545

    add pos tagger with spacy

--- nlp/POS.py
+++ nlp/POS.py
@@ -6,6 +6,7 @@ import nltk
 import re
 from nltk import pos_tag
 from collections import defaultdict
+import spacy
 
 # first tkenize the words into individual tokens
 
@@ -26,17 +27,41 @@ def word_tokenize(word):
     word = filter(None, word)
     return list(word)
 
+
+def create_dict(tagged):
+    """
+        this function create the dictionary of tagged words
+    """
+    tags_lookup = defaultdict(lambda: list())
+
+    for token, tags in tagged:
+        tags_lookup[tags] += [token]
+    
+    return tags_lookup
+
+
 # to pass the word to pos tagger we need to tokenize the words
 tokens = word_tokenize(word)
-print()
 
 # Now tag the word with nltk default pos tagger 
 tagged = pos_tag(tokens)
 
 # lets create dict from list of tuples which will look preety and follow python standered
-tags_lookup = defaultdict(lambda: list())
+tags_lookup = create_dict(tagged)
 
-for token, tags in tagged:
-    tags_lookup[tags] += [token]
+print('Part of Speech tagging with NLTK default Tagger')
+print('-----------------------------------------')
+print(tags_lookup)
 
+# Now using spacy to generate pos tags of the sentence 
+
+nlp = spacy.load('en_core_web_sm')
+doc = nlp(word)
+
+tagged = [(token.text, token.pos_) for token in doc]
+
+tags_lookup = create_dict(tagged)
+
+print('Part of Speech tagging with spacy default Tagger')
+print('-----------------------------------------')
 print(tags_lookup)
