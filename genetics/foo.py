f48b59a258eba9bdd4edee382fdd49db38be22c2
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2019-08-05 14:11:24 +0545

    add python program for getting all class defines inside module

---
+++ genetics/foo.py
@@ -0,0 +1,26 @@
+import sys
+import inspect
+
+class Spare(object):
+    '''
+        This class will preserve the most fundamental works of 
+        python codementality
+    '''
+    pass
+
+
+class Container(Spare):
+    '''
+        This is the container to store all the useful information of
+        python class that will be later useful
+    '''
+
+    def __init__(self, obj, docs):
+        self.cls = obj
+        self.docs = docs
+        return super().__init__()
+
+
+if __name__ == "__main__":
+    clsmembers = inspect.getmembers(sys.modules[__name__] , inspect.isclass)
+    print(clsmembers)
