d504061f760a0cd99cfbbff506cae4d661039716
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2019-09-05 10:15:00 +0545

    add google package to make automation easy

---
+++ google/README.md
@@ -0,0 +1,9 @@
+# Descent Py
+
+## Google
+
+---------
+
+### About
+
+This package consist of scripts which will make google works like upload file to drive, downloading google docs, google sheets, uploading google sheets easy and automated.
---
+++ google/__init__.py
@@ -0,0 +1,14 @@
+
+"""
+                         _      
+  __ _  ___   ___   __ _| | ___ 
+ / _` |/ _ \ / _ \ / _` | |/ _ \
+| (_| | (_) | (_) | (_| | |  __/
+ \__, |\___/ \___/ \__, |_|\___|
+ |___/             |___/        
+
+Author: Aaditya Chapagain
+version: 1.0.1
+
+description: scripts which will make google API's easy in python.
+"""
