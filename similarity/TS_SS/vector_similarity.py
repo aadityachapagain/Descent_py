901b93fff3ef981bcd1476c85ba4d69f074affca
aaditya <aaditya@fusemachines.com>
2019-10-15 17:46:48 +0545

    add TS_SS similarity as class and support multidimentional array

--- similarity/TS_SS/vector_similarity.py
+++ similarity/TS_SS/vector_similarity.py
@@ -1,31 +1,33 @@
-import numpy as np
 import math
+import numpy as np
 
-def Cosine(vec1: np.ndarray, vec2: np.ndarray):
-    return np.dot(vec1, vec2.T)/(np.linalg.norm(vec1) * np.linalg.norm(vec2))
+class TS_SS:
+    
+    def Cosine(self, vec1: np.ndarray, vec2: np.ndarray):
+        return np.dot(vec1, vec2.T)/(np.linalg.norm(vec1) * np.linalg.norm(vec2))
 
-def VectorSize(vec: np.ndarray):
-    return np.linalg.norm(vec)
+    def VectorSize(self, vec: np.ndarray):
+        return np.linalg.norm(vec)
 
-def Euclidean(vec1: np.ndarray, vec2: np.ndarray):
-    return np.linalg.norm(vec1-vec2)
+    def Euclidean(self, vec1: np.ndarray, vec2: np.ndarray):
+        return np.linalg.norm(vec1-vec2)
 
-def Theta(vec1: np.ndarray, vec2: np.ndarray):
-    return np.arccos(Cosine(vec1, vec2)) + np.radians(10)
+    def Theta(self, vec1: np.ndarray, vec2: np.ndarray):
+        return np.arccos(self.Cosine(vec1, vec2)) + np.radians(10)
 
-def Triangle(vec1: np.ndarray, vec2: np.ndarray):
-    theta = np.radians(Theta(vec1, vec2))
-    return (VectorSize(vec1) * VectorSize(vec2) * np.sin(theta))/2
+    def Triangle(self, vec1: np.ndarray, vec2: np.ndarray):
+        theta = np.radians(self.Theta(vec1, vec2))
+        return (self.VectorSize(vec1) * self.VectorSize(vec2) * np.sin(theta))/2
 
-def Magnitude_Difference(vec1: np.ndarray, vec2: np.ndarray):
-    return np.abs(VectorSize(vec1) - VectorSize(vec2))
+    def Magnitude_Difference(self, vec1: np.ndarray, vec2: np.ndarray):
+        return abs(self.VectorSize(vec1) - self.VectorSize(vec2))
 
-def Sector(vec1: np.ndarray, vec2: np.ndarray):
-    ED = Euclidean(vec1, vec2)
-    MD = Magnitude_Difference(vec1, vec2)
-    theta = Theta(vec1, vec2)
-    return math.pi * ((ED + MD)**2) * theta/360
+    def Sector(self, vec1: np.ndarray, vec2: np.ndarray):
+        ED = self.Euclidean(vec1, vec2)
+        MD = self.Magnitude_Difference(vec1, vec2)
+        theta = self.Theta(vec1, vec2)
+        return math.pi * (ED + MD)**2 * theta/360
 
 
-def TS_SS(vec1: np.ndarray, vec2: np.ndarray):
-    return Triangle(vec1, vec2) * Sector(vec1, vec2)
+    def __call__(self, vec1: np.ndarray, vec2: np.ndarray):
+        return self.Triangle(vec1, vec2) * self.Sector(vec1, vec2)
