3217fb7d7f4d3489fa0ca8ecd1c167893afa5df8
aaditya <aaditya@fusemachines.com>
2019-10-15 22:00:55 +0545

    add docs for TS_SS:

--- similarity/TS_SS/vector_similarity.py
+++ similarity/TS_SS/vector_similarity.py
@@ -2,6 +2,26 @@ import math
 import numpy as np
 
 class TS_SS:
+    """
+    Triangular Section and Sector section Similarity
+
+    Cosine similarity measure the angle between the two vectors which incase
+    of General NLP task might work just fine. But cosine similarity might not
+    able to measure the frequency of words  in general. As In NLP the resulting
+    embedding of words vocab for any large data vector in any particular space
+    have direction magnitude from origin. Where direction gives the group that 
+    particular vector lies in and magnitude gives frequncy of particular words
+    of that group. Hence Cosine similarty failed to take account of word frequceny
+    which is actually import in various NLP tasks.
+
+    So, TS_SS takes account both the direction , magnitude of vecors and triangular sections
+    vecorts which we are calculating the similarty. While TS_SS consists of area under triangular
+    section and magnitude difference of two vectors. It might take account all the possible features
+    of vectors.
+
+    Unlike the Cosine similarty. Its values ranges from 0 to Inifnity
+    
+    """
     
     def Cosine(self, vec1: np.ndarray, vec2: np.ndarray):
         return np.dot(vec1, vec2.T)/(np.linalg.norm(vec1) * np.linalg.norm(vec2))
