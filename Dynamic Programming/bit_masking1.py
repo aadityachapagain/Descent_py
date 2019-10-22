c1bd09e855d0a1c97fade92db8d91494c1c80cc1
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2019-10-22 09:18:13 +0545

    add description to the bitmasking program

--- Dynamic Programming/bit_masking1.py
+++ Dynamic Programming/bit_masking1.py
@@ -1,3 +1,15 @@
+
+"""
+Consider the below problems statement.
+
+There are 100 different types of caps each having a unique id from 1 to 100. Also,
+there are ‘n’ persons each having a collection of a variable number of caps.
+One day all of these persons decide to go in a party wearing a cap but to look unique
+they decided that none of them will wear the same type of cap. So, count the total number
+of arrangements or ways such that none of them is wearing the same type of cap.
+
+"""
+
 #Python program to find number of ways to wear hats 
 from collections import defaultdict 
   
